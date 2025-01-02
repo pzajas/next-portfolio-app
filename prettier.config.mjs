d28d481e3f1363833d402be97fe0fc1aa9c809eb
pzajas <zajas.piotr@gmail.com>
2025-01-02 22:58:54 +0100

    Change prettier config.

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,22 +1,15 @@
-const prettierConfig =
-  {
-    arrowParens:
-      'always',
-    bracketSpacing: true,
-    jsxSingleQuote: false,
-    quoteProps:
-      'as-needed',
-    singleQuote: true,
-    semi: false,
-    printWidth: 120,
-    useTabs: false,
-    tabWidth: 2,
-    trailingComma:
-      'none',
-    plugins:
-      [
-        'prettier-plugin-classnames'
-      ]
-  }
+const prettierConfig = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  semi: false,
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  plugins: ['prettier-plugin-classnames']
+}
 
 export default prettierConfig
